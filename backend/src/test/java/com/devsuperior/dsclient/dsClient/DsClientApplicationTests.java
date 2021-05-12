77918ab8cc778930bb679dd763b30726bb2250b8
fabriciodelima06 <fabriciodelima06@gmail.com>
2021-05-12 20:52:06 -0300

    project created, h2, entity

---
+++ backend/src/test/java/com/devsuperior/dsclient/dsClient/DsClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsuperior.dsclient.dsClient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
